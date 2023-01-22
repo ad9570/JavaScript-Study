c14a08bdf06b3be39b82181093a17f6dbd0370f8
MinWooPark <narris64@gmail.com>
2023-01-22 23:04:18 +0900

    function

--- js002/assets/scripts/app.js
+++ js002/assets/scripts/app.js
@@ -3,7 +3,15 @@ alert(message);
 
 const initValue = 0;
 let result = initValue;
-result =  result + 10 * 2 / (5 - 1);
+
+function add(num1, num2) {
+    const addResult = num1 + num2;
+    return addResult;
+}
+// alert(num1);    // 매개변수는 함수 내에서만 유효
+// alert(addResult);   // 함수 내에서 선언된 변수/상수는 함수 내에서만 유효
+
+result = add(1, 2);
 
 // let log = '0 + 10 * 2 / (5 - 1)';       // 수학 연산을 실행하지 않음
 // let log = 'result + 10 * 2 / (5 - 1)';  // ''(따옴표)로 인해 JavaScript가 안의 내용을 숫자, 변수가 아닌 정적 텍스트로 인식
@@ -11,18 +19,3 @@ result =  result + 10 * 2 / (5 - 1);
 let log = `${initValue} + 10 * 2 / (5 - 1)`;    // 템플릿 리터럴
 
 outputResult(result, log);
-
-let errorMessage1 = 'An error '
-                    + 'occured!';
-let errorMessage2 = 'An error\n'
-                    + 'occured!';
-let errorMessage3 = `An error 
-                    occured!`;
-let errorMessage4 = `An error
-occured!`;
-let errorMessage5 = `An error\noccured!`
-document.getElementById('error1').textContent = errorMessage1;
-document.getElementById('error2').textContent = errorMessage2;
-document.getElementById('error3').textContent = errorMessage3;
-document.getElementById('error4').textContent = errorMessage4;
-document.getElementById('error5').textContent = errorMessage5;
--- js002/assets/scripts/vendor.js
+++ js002/assets/scripts/vendor.js
@@ -11,3 +11,19 @@ function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
+
+let errorMessage1 = 'An error '
+    + 'occured!';
+let errorMessage2 = 'An error\n'
+    + 'occured!';
+let errorMessage3 = `An error 
+                    occured!`;
+let errorMessage4 = `An error
+occured!`;
+let errorMessage5 = `An error\noccured!`;
+
+document.getElementById('error1').textContent = errorMessage1;
+document.getElementById('error2').textContent = errorMessage2;
+document.getElementById('error3').textContent = errorMessage3;
+document.getElementById('error4').textContent = errorMessage4;
+document.getElementById('error5').textContent = errorMessage5;
