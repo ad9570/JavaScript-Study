7ed217c46866aaf31f08f480ad7c697b910b1a44
MinWooPark <narris64@gmail.com>
2023-01-25 23:10:22 +0900

    EventListener

--- js002/assets/scripts/app.js
+++ js002/assets/scripts/app.js
@@ -4,18 +4,20 @@ alert(message);
 const initValue = 0;
 let result = initValue;
 
-function add(num1, num2) {
+function addTest(num1, num2) {
     const addResult = num1 + num2;
     return addResult;
 }
-// alert(num1);    // 매개변수는 함수 내에서만 유효
-// alert(addResult);   // 함수 내에서 선언된 변수/상수는 함수 내에서만 유효
+alert(addTest(1, 3));
 
-result = add(1, 2);
+function add() {
+    result = result + userInput.value;
+    outputResult(result, '');
+}
+
+addBtn.addEventListener('click', add);
 
 // let log = '0 + 10 * 2 / (5 - 1)';       // 수학 연산을 실행하지 않음
 // let log = 'result + 10 * 2 / (5 - 1)';  // ''(따옴표)로 인해 JavaScript가 안의 내용을 숫자, 변수가 아닌 정적 텍스트로 인식
 // let log = initValue + ' + 10 * 2 / (5 - 1)';
-let log = `${initValue} + 10 * 2 / (5 - 1)`;    // 템플릿 리터럴
-
-outputResult(result, log);
+// let log = `${initValue} + 10 * 2 / (5 - 1)`;    // 템플릿 리터럴
