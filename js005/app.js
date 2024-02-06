7f50df3c604d3cde55d7a3d68b692db76d274537
MinWooPark <narris64@gmail.com>
2024-02-07 00:06:18 +0900

    anonymous function

---
+++ js005/app.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const startGame = function () {
+    console.log('Game is starting...');
+};
+
+startGameBtn.addEventListener('click', startGame);
