eb03c2e8ab20c7eb6860759373007f46d852cede
MinWooPark <narris64@gmail.com>
2024-02-07 23:59:23 +0900

    anonymous func

--- js005/app.js
+++ js005/app.js
@@ -1,7 +1,34 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const startGame = function () {
-    console.log('Game is starting...');
-};
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
 
-startGameBtn.addEventListener('click', startGame);
+let isRunning = false;
+
+const getPlayerChoice = function () {
+    let selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '');
+    if (selection) {
+        selection = selection.toUpperCase();
+    }
+
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        const randomInteger = Math.floor(Math.random() * 3);
+        const randomChoice = randomInteger === 0 ? ROCK : randomInteger === 1 ? PAPER : SCISSORS;
+
+        alert(`Invalid choice! We chose ${randomChoice} for you!`);
+        return randomChoice;
+    }
+
+    return selection;
+}
+startGameBtn.addEventListener('click', function () {
+    if (isRunning) {
+        return;
+    }
+
+    isRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log('playerSelection : ' + playerSelection);
+});
