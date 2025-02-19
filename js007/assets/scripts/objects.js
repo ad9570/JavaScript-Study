3e1f862d64332bbcb0133f3bf0966be190716477
pmw <narris64@gmail.com>
2025-02-20 00:14:25 +0900

    render movies

--- js007/assets/scripts/objects.js
+++ js007/assets/scripts/objects.js
@@ -1,7 +1,37 @@
 const addMoviewBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
+const addMovieInputs = document.getElementById('user-input').querySelectorAll('input');
 let id = 1;
 const movies = [];
+
+const renderMovies = () => {
+    const movieList = document.getElementById('movie-list');
+    movieList.innerHTML = '';
+
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+
+    movies.forEach(movie => {
+        const movieItem = document.createElement('li');
+        let title;
+        let extraInfo;
+        for (const infoKey in movie.info) {
+            if (infoKey === 'title') {
+                title = movie.info[infoKey];
+            } else {
+                extraInfo = `${infoKey}: ${movie.info[infoKey]}`;
+            }
+        }
+        movieItem.textContent = `${title} | ${extraInfo}`;
+        movieList.append(movieItem);
+    });
+
+    addMovieInputs.forEach(input => input.value = '');
+};
 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
@@ -23,6 +53,18 @@ const addMovieHandler = () => {
 
     movies.push(newMovie);
     id++;
+
+    renderMovies();
+};
+
+const keyEvents = {
+    addMovie: e => {
+        if (e.keyCode === 13) {
+            addMovieHandler();
+        }
+    }
 };
 
 addMoviewBtn.addEventListener('click', addMovieHandler);
+
+addMovieInputs.forEach(input => input.addEventListener('keyup', keyEvents.addMovie));
